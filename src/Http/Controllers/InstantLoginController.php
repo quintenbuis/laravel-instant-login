5eb7bd77b262adda9f049fe4a5e52f76ceb4b3fe
Quinten Buis <quinten@justbetter.nl>
2021-07-23 23:20:24 +0200

    wip

--- src/Http/Controllers/InstantLoginController.php
+++ src/Http/Controllers/InstantLoginController.php
@@ -2,11 +2,9 @@
 
 namespace Quintenbuis\InstantLogin\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class InstantLoginController
 {
-    public function __invoke(Request $request)
+    public function __invoke()
     {
         // TODO: Figure out how to authenticate model and have it persist
         return redirect('/dashboard');
