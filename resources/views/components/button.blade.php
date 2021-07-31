e5b9b32f7e1cc2979b26a8bbf42531c927ed4c71
Quinten Buis <quinten@justbetter.nl>
2021-07-31 12:49:46 +0200

    Only show button in non production modes

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,3 +1,5 @@
-<button onclick="location.href = '{{ route(config('instant-login.route.name')) }}'" {{ $attributes->merge(['class' => 'mt-4 w-full px-3 py-2 border border-gray-300 shadow-sm text-sm leading-4 font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500']) }}>
-    @lang('Dev Login')
-</button>
+@env(['local', 'staging'])
+    <button onclick="location.href = '{{ route(config('instant-login.route.name')) }}'" {{ $attributes->merge(['class' => 'mt-4 w-full px-3 py-2 border border-gray-300 shadow-sm text-sm leading-4 font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500']) }}>
+        @lang('Dev Login')
+    </button>
+@endenv
